0f048e8d51670d84934cd652730f3c33fd61d8a0
GlaudistonNeto <glaudistonson@gmail.com>
2022-01-01 21:49:18 -0300

    added v-if, v-show and v-bind

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <p>I'm working at this moment.</p>
+    <p v-if="isWorking">I'm working at this moment.</p>
+    <p v-else>I'm looking for a job at this moment.</p>
     <p>I use the following technologies:</p>
     <ul>
       <li>JavaSript</li>
@@ -8,12 +9,33 @@
       <li>Vue.js</li>
       <li>React-native</li>
     </ul>
+    <!-- <p v-if="!isWorking">Send a mail to: {{ email }}</p> -->
+    <p v-show="showEmail">Send a mail to: {{ email }}</p>
+    <p>To access my portfolio 
+      <a v-bind:href="portfolio" target="_blank">
+      click here
+      </a>
+    </p>
+    <Picture />
   </div>
 </template>
 
 <script>
+import Picture from './Picture.vue';
+
 export default {
   name: 'Info',
+  data() {
+    return {
+      isWorking: false,
+      showEmail: true,
+      email: 'glaudistonson@gmail.com',
+      portfolio: 'https://www.upwork.com/freelancers/~01e098fb79f01796b4'
+    }
+  },
+  components: {
+    Picture,
+  }
 }
 </script>
 
---
+++ src/components/Picture.vue
@@ -0,0 +1,19 @@
+<template>
+  <img :src="avatar" :alt="description">
+</template>
+
+<script>
+export default {
+  name: 'Picture',
+  data() {
+    return {
+      avatar: "/img/avatar.png",
+      description: "G-Neto"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
